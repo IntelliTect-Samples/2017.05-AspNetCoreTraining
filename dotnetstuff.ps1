3837c5863b214128b70586edc6b3f12aa7ff3440
Mark Michaelis <mark@IntelliTect.com>
2017-05-18 18:06:04 -0700

    Updated to use default solution name and specific project name; Changed to create a specific solution directory.

--- dotnetstuff.ps1
+++ dotnetstuff.ps1
@@ -1,9 +1,10 @@
 [CmdletBinding()]
 param(
     [switch]$clean,
-    $solutionName = 'AspNetCoreStuff',
-    $projectName = 'DotNetAspNetMvc',
-    [switch]$startDebug =(-not (Get-PSBreakpoint -Script (Split-Path $MyInvocation.MyCommand.Path -Leaf)))
+    [string]$solutionName = 'DotNetCliStuff',
+    [string]$projectName = 'SampleWebApplication',
+    [string]$rootDirectory = $pwd,
+    [switch]$startDebug
 )
 
 Function Stop-DemoDebugging {
@@ -12,30 +13,50 @@ Function Stop-DemoDebugging {
 }
 
 Function Start-DemoDebugging {
+    Stop-DemoDebugging
     Set-PSBreakpoint -Script (__FILE__) -Line (Get-DemoStartLine);
     return
 }
 
 Function Undo-Demo {
-    Remove-Item "$PSScriptRoot\$solutionName.sln" -ErrorAction ignore
-    Remove-Item $pwd\$projectName -Recurse -Force -ErrorAction ignore
+    Remove-Item "$rootDirectory\$solutionName" -Recurse -Force -ErrorAction ignore
+    Remove-Item $rootDirectory\$projectName -Recurse -Force -ErrorAction ignore
 }
 
-Function Get-DemoStartLine { return ((__LINE__) + 2)}
-Function Start-Demo {
+
+Function Show-DotNetCliInformation {
     dotnet.exe
     Get-Command dotnet | % Source
     dotnet --version
     get-childitem "$env:ProgramFiles\dotnet\sdk"
+}
 
-    dotnet new sln --name $solutionName
+Function Get-DemoStartLine { return ((__LINE__) + 2)}
+Function Start-Demo {
+    Show-DotNetCliInformation
+    Function New-DotNetSolutionDirectory ($solutionName) {
+        $solutionDirectory = (Join-Path $rootDirectory $solutionName)
+        Write-Debug $solutionDirectory
+        New-Item -ItemType Directory $solutionDirectory >> $null
+        Set-Location $solutionDirectory
+        return $solutionDirectory
+    }
+    
+    try {
+        $originalDirectory = $pwd; Set-Location (New-DotNetSolutionDirectory $solutionName) 
+        dotnet new sln;                             
+        # Test-Path (Get-Item "$(Split-Path (Get-Location) -Leaf).sln")  
+        Get-Item "$solutionName.sln"
+        dotnet new mvc -n "$projectName";              
+        # Test-Path (Get-Item "$projectName") 
+        Get-ChildItem $projectName
+    }
+    finally {
+        Set-Location $originalDirectory
+    }
 
     #Remove-Item "$pwd\$(Split-Path (Get-Location) -Leaf).sln" -ErrorAction ignore
     
-    dotnet new mvc -n $projectName
-    Get-ChildItem ".\$projectName"
-
-    dotnet sln ".\$solutionName.sln" add ".\$projectName\$projectName.csproj"
 }
 
 Function Get-ScriptLineNumber { return (Get-PSCallStack)[1].ScriptLineNumber }
@@ -43,20 +64,11 @@ Set-Alias -Name __LINE__ -value Get-ScriptLineNumber >> $null
 Function Get-ScriptName { return (Get-PSCallStack)[1].ScriptName }
 Set-Alias -Name __FILE__ -value Get-ScriptName >> $null 
 
-Function New-DotNetSolutionUsingDirectoryName {
-    [CmdletBinding()]
-    Param($projectDirectory)
-
-    try {
-        Set-Location "$PSScriptRoot\$projectDirectory"
-        dotnet new sln
-        Test-Path (Get-Item "$pwd\$(Split-Path (Get-Location) -Leaf).sln")
-    }
-    finally {
-        Set-Location $PSScriptRoot
-    }
+if (-not $PSBoundParameters.ContainsKey('startDebug')) {
+#    (Split-Path (__FILE__) -Leaf)
+#    (Get-PSBreakpoint -Script (Split-Path __FILE__ -Leaf))
+    $startDebug = -not (Get-PSBreakpoint -Script (Split-Path (__FILE__) -Leaf))
 }
-
 
 if($clean)
 {
