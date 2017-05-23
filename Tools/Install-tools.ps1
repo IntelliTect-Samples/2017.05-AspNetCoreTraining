8824335f3dfc1233285dabfd025fa92939f894e1
Mark Michaelis <mark@IntelliTect.com>
2017-05-24 02:16:40 +0300

    Added Microsoft.VisualStudio.RazorExtension.vsix

---
+++ Tools/Install-tools.ps1
@@ -0,0 +1,2 @@
+invoke-webrequest -Uri 'https://marketplace.visualstudio.com/items?itemName=ms-madsk.RazorLanguageServices'  -outfile Microsoft.VisualStudio.RazorExtension.vsix
+& Microsoft.VisualStudio.RazorExtension.vsix
