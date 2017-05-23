97d10b7cb00041cb529b51cd913d6c9354e9ed03
Mark Michaelis <mark@IntelliTect.com>
2017-05-24 02:56:32 +0300

    Added install pseudo script.

--- Tools/Install-tools.ps1
+++ Tools/Install-tools.ps1
@@ -1,2 +1,9 @@
+return
+
+# Mock Sample for the Install:
+
 invoke-webrequest -Uri 'https://marketplace.visualstudio.com/items?itemName=ms-madsk.RazorLanguageServices'  -outfile Microsoft.VisualStudio.RazorExtension.vsix
+
+set-alias VSIXInstaller "${env:ProgramFiles(x86)}\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe"
+
 & Microsoft.VisualStudio.RazorExtension.vsix
