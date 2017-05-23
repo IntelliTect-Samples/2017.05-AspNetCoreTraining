6e62b788a457f38e35117acedd736ed2c6ddccc6
Mark Michaelis <mark@IntelliTect.com>
2017-05-24 01:53:33 +0300

    Added update file.

--- Update-06.ps1
+++ Update-06.ps1
@@ -1,5 +1,1 @@
-dotnet add package Microsoft.Aspnetcore.StaticFiles
-
-Set-Alias bower "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Enterprise\Web\External\bower.cmd"
-bower install bootstrap#3.3.7 --force-latest
-
+dotnet new viewstart -o .\Views\
